7d90db7518ae181f4c3ac90eddd482871be1b753
ogane0112 <kai513272@gmail.com>
2024-09-20 08:43:16 +0900

    supabaseに関する設定を消した

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,29 +18,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="ja" suppressHydrationWarning>
       <body className="bg-background text-foreground">
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <main className="min-h-screen flex flex-col items-center">
-            <div className="flex-1 w-full flex flex-col gap-20 items-center">
-              <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-                <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
-                  <div className="flex gap-5 items-center font-semibold">
-                    <Link href={"/"}>Next.js Supabase Starter</Link>
-                  </div>
-                </div>
-              </nav>
-              <div className="flex flex-col gap-20 max-w-5xl p-5">
                 {children}
-              </div>
-            </div>
-          </main>
-        </ThemeProvider>
       </body>
     </html>
   );
